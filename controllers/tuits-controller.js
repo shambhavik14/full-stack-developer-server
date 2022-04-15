c0dcf8590485790021f63506bf465b128c9212c6
shambhavik14 <68625339+shambhavik14@users.noreply.github.com>
2022-04-15 00:15:42 -0400

    Update tuits-controller.js

--- controllers/tuits-controller.js
+++ controllers/tuits-controller.js
@@ -1,51 +1,41 @@
-import tuitsDao from "../tuits/tuits-dao.js";
+import posts from "./users/tuits.js";
+let tuits = posts;
 
 export default (app) => {
  app.post('/api/tuits', createTuit);
- app.get('/api/tuits', findAllTuits);
- app.put('/api/tuits/:tid', updateTuit);
- app.delete('/api/tuits/:tid', deleteTuit);
+	@@ -9,34 +8,34 @@ export default (app) => {
  app.put('/api/tuits/:tid', dislikeTuit);
 }
 
-const createTuit = async (req, res) => {
+const createTuit = (req, res) => {
  const newTuit = req.body;
- const insertedTuit = await tuitsDao.createTuit(newTuit);
+ newTuit._id = (new Date()).getTime()+'';
  newTuit.image = "../../images/susie.jpg";
  newTuit.handle = "susieandassociates";
  newTuit.author = "Susie Mayerson";
  newTuit.likes = 0;
  newTuit.dislikes = 5;
- res.json(insertedTuit);
+ tuits.push(newTuit);
+ res.json(newTuit);
 }
 
 
-const findAllTuits = async (req, res) => {
-const tuits = await tuitsDao.findAllTuits()
+const findAllTuits = (req, res) => {
  res.json(tuits);
 }
 
-const deleteTuit = async (req, res) => {
+const deleteTuit = (req, res) => {
  const tuitdIdToDelete = req.params.tid;
- const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
- res.send(status);
+ tuits = tuits.filter(t => t._id !== tuitdIdToDelete);
+ res.sendStatus(200);
 }
 
 
-const updateTuit = async (req, res) => {
+const updateTuit = (req, res) => {
  const tuitdIdToUpdate = req.params.tid;
  const updatedTuit = req.body;
- const status = await tuitsDao.updateTuit(tuitdIdToUpdate, updatedTuit);
+ tuits = tuits.map(t => t._id === tuitdIdToUpdate ? updatedTuit : t);
  res.sendStatus(200);
 }
-
-const dislikeTuit = (req, res) => {
- const tuitdIdToDislike = req.params.tid;
- const dislikedTuit = req.body;
- tuits = tuits.map(t => t._id === tuitdIdToDislike ? dislikedTuit : t);
- res.sendStatus(200);
-}
-
-
 
 
