0674e504a2d3aa5072ccc2edede11a6a7f4b5370
shambhavik14 <68625339+shambhavik14@users.noreply.github.com>
2022-04-16 16:47:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import tuitsController   from "./controllers/tuits-controller.js";
 const DB_CONNECTION_STRING = 'mongodb+srv://giuseppi:supersecretpassword@cluster0.psger.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
  || 'mongodb://localhost:27017/webdev'
-mongoose.connect("mongodb://localhost:27017/webdev");
+mongoose.connect(CONNECTION_STRING);
 
 
 const app = express();
