4cd56be1c01f2788a6176e2b948fbd7b1c633731
shambhavik14 <68625339+shambhavik14@users.noreply.github.com>
2022-04-20 18:36:13 -0400

    Update tuits-dao.js

--- tuits/tuits-dao.js
+++ tuits/tuits-dao.js
@@ -1,9 +1,9 @@
 import tuitsModel from './tuits-model.js';
 
 export const findAllTuits = () => tuitsModel.find();
-export const createTuit = (tuits) => tuitsModel.create(tuits);
+export const createTuit = (tuit) => tuitsModel.create(tuit);
 export const deleteTuit = (tid) => tuitsModel.deleteOne({_id: tid});
-export const updateTuit = (tid, tuits) => tuitsModel.updateOne({_id: tid}, {$set: tuits})
+export const updateTuit = (tid, tuit) => tuitsModel.updateOne({_id: tid}, {$set: tuit})
 
 
 
